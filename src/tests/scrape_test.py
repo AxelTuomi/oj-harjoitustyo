08bb3d665a587b0b066770076e8284fc34629192
Axel Tuomi <axeltuomi21@gmail.com>
2022-04-05 23:56:08 +0300

    fixed typo

--- src/tests/scrape_test.py
+++ src/tests/scrape_test.py
@@ -2,11 +2,11 @@ import unittest
 from index import Scrape
 
 class TestScrape(unittest.TestCase):
-    def test_loading_website(self)
+    def test_loading_website(self):
         self.load_website(self, "https://unicafe.fi/sv/restaurants/exactum/")
 	self.assertTrue(self.page != "")
 
-    def test_searching_webiste
+    def test_searching_webiste(self):
         self.load_website(self, "https://unicafe.fi/sv/restaurants/exactum/")
 	self.assertTrue(self.search_for_ingredient("Rypsiöljy") > 0)
         

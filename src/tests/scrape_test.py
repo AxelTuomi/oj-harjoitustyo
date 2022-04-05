bd19610e507435606cd082774490d8c3edb8f4f7
Axel Tuomi <axeltuomi21@gmail.com>
2022-04-05 23:58:52 +0300

    tab error

--- src/tests/scrape_test.py
+++ src/tests/scrape_test.py
@@ -4,7 +4,7 @@ from index import Scrape
 class TestScrape(unittest.TestCase):
     def test_loading_website(self):
         self.load_website(self, "https://unicafe.fi/sv/restaurants/exactum/")
-	self.assertTrue(self.page != "")
+        self.assertTrue(self.page != "")
 
     def test_searching_webiste(self):
         self.load_website(self, "https://unicafe.fi/sv/restaurants/exactum/")

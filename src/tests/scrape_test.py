875b2e2ce19fe7e8fdbb417d68bed4edfe596f81
Axel Tuomi <axeltuomi21@gmail.com>
2022-04-05 23:52:48 +0300

    kehitin sovellusta

---
+++ src/tests/scrape_test.py
@@ -0,0 +1,12 @@
+import unittest
+from index import Scrape
+
+class TestScrape(unittest.TestCase):
+    def test_loading_website(self)
+        self.load_website(self, "https://unicafe.fi/sv/restaurants/exactum/")
+	self.assertTrue(self.page != "")
+
+    def test_searching_webiste
+        self.load_website(self, "https://unicafe.fi/sv/restaurants/exactum/")
+	self.assertTrue(self.search_for_ingredient("Rypsiöljy") > 0)
+        
